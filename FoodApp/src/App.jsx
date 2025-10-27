d9a8da470de57039d8c9460b314b886c0b847ab0
bright charway <charway30@gmail.com>
2025-10-27 11:26:54 +0100

    useEffect hook

--- FoodApp/src/App.jsx
+++ FoodApp/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Search from './component/Search'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <Search/>
       
     </div>
   )
